12a7d3af3708e0b7f98e8b750db3a9043578109c
ChpOlzhik <57743402+ChpOlzhik@users.noreply.github.com>
2021-05-24 17:13:26 +0600

    Cleaned code

--- huffman.py
+++ huffman.py
@@ -44,53 +44,6 @@ class Huffman:
         except Exception as e:
             print("Unknown error: ", e)
 
-    def __GenerateHamming(self, bits):
-        try:
-            data = list(bits)
-            m = len(data)
-            c = 0
-            j = 0
-            result = []
-            # m + r + 1 = 2**r , r - redunant bits, m - bits of encoded code
-            r = 0
-            while m + r + 1 > pow(2, r): # Calculating number of redunant bits
-                r += 1
-
-            if r == 0:
-                print(str(data))
-
-            for i in range(r + m): #Adding redunant bits
-                p = (2 ** c)
-                if (p == (i + 1)):
-                    result.append(0)
-                    c = c + 1
-                else:
-                    result.append(int(data[j]))
-                    j = j + 1
-
-
-            matrix = []
-            cur = 0
-            for i in range(m+r):
-                cur = cur+1
-                binar = bin(cur)[2:]
-                while len(binar) != r:
-                    binar = '0'+binar
-
-                matrix.append(list(reversed(binar)))
-            c = 0
-
-            while 2**c < m + r:
-                ans = 0
-                for i in range(m+r):
-                    ans += int(result[i]) * int(matrix[i][c])
-                result[2**c - 1] = ans % 2
-                c += 1
-
-            ans = ''.join(map(str, result))
-            return ans
-        except Exception as e:
-            print("Unknown error on generating hamming code: ", e)
 
     def __makeHeap(self, freq):
         try:
@@ -123,21 +76,79 @@ class Huffman:
         except Exception as e:
             print("Unknown error on makeCodes", e)
 
+    def __generateMatrix(self, m , r, revers=False):
+        matrix = []
+        cur = 0
+        for i in range(m + r):
+            cur = cur + 1
+            binar = bin(cur)[2:]
+            while len(binar) != r:
+                binar = '0' + binar
+            if revers:
+                matrix.append(list(reversed(binar)))
+            else:
+                matrix.append(list(binar))
+        return matrix
+
+    def __generateParityBits(self, data, matrix): #generating redunant bits using matrix
+        c = 0
+        pbits = []
+        while 2 ** c < len(data):
+            ans = 0
+            for i in range(len(data)):
+                ans += int(data[i]) * int(matrix[i][c])
+            pbits.append(ans % 2)
+            c += 1
+        return pbits
+
+
+    def __GenerateHamming(self, bits):
+        try:
+            data = list(bits)
+            m = len(data)
+            c, j, r = 0, 0, 0
+            result = []
+            while m + r + 1 > pow(2, r): # Calculating number of redunant bits
+                r += 1
+
+            if r == 0:
+                ans = ''.join(map(str, data))
+                return ans
+
+            for i in range(r + m): #Adding redunant bits
+                p = (2 ** c)
+                if (p == (i + 1)):
+                    result.append(0)
+                    c = c + 1
+                else:
+                    result.append(int(data[j]))
+                    j = j + 1
+
+            matrix = self.__generateMatrix(m, r, True) #making matrix
+            pbits = self.__generateParityBits(result, matrix) #finding values of parity bits
+            for i in range(r):
+                result[2**i - 1] = pbits[i] #assigning perity bits
+
+            ans = ''.join(map(str, result))# converting to string
+            return ans
+        except Exception as e:
+            print("Unknown error on generating hamming code: ", e)
+
     def __writeResult(self, txt):
         try:
             ans = ''
             for i in txt:
                 ans += self.__codes[i]
 
-
             with open('encoded_text.txt', 'w') as fil:
                 fil.write(ans)
+            print("Huffman encoded: ", ans)
             m = len(ans)
 
             res = ''
             for i in range(0, len(ans),4):
                 res += self.__GenerateHamming(ans[i:(i+4)])
-
+            print("Hamming encoded: ", res)
             with open('hamming.txt', 'w') as fil:
                 fil.write(res)
 
@@ -148,6 +159,7 @@ class Huffman:
         try:
             with open(fileName, 'r') as file:
                 txt = file.read()
+                print("Text: ", txt)
                 freq = self.__getFreq(txt)
 
             self.__makeHeap(freq)
@@ -160,7 +172,7 @@ class Huffman:
         except Exception as e:
             print("Unknown error: ", e)
 
-    def __makeError(self):
+    def __makeError(self): #makinging an error for hamming encoding
         with open('hamming.txt', 'r') as file:
             txt = file.read()
             txt = txt.strip()
@@ -171,7 +183,7 @@ class Huffman:
             if l != len(x):
                 x[l] = str(1 - int(x[l]))
             res += ''.join(map(str, x))
-
+        print("Hamming encoded with errors: ", res)
         with open('WithErrors.txt', 'w') as file:
             file.write(res)
 
@@ -196,77 +208,52 @@ class Huffman:
                         cur = root
                     cur = cur.right
             res += cur.char
-            # print(res)
-
             with open('decoded_text.txt', 'w') as file:
                 file.write(res)
+
             self.__makeError()
-
-            # Error correcting of hamming
-            with open('WithErrors.txt', 'r') as file:
-                txt = file.read()
-                txt = txt.strip()
-            print(txt)
-
-            res = ''
-            for i in range(0, len(txt), 7):
-                x = list(txt[i:(i + 7)])
-                # print(x)
-                cur = self.__decode_hamming(x)
-                # print(list(cur),'\n')
-                res += cur
-            print(res)
-            with open('decoded_hamming.txt', 'w') as file:
-                file.write(res)
+            self.__HammingErrorCorrection()
 
         except Exception as e:
             print('Error in decoding', e)
 
-    def __decode_hamming(self, data):
+    def __HammingBlockCorrection(self, startIndex, data):
         try:
             m = len(data)
             r = 0
-            while 2**r < m:
+            while 2**r < m: # getting value of r(redunant bits)
                 r += 1
+            # getting matrix
+            matrix = self.__generateMatrix(m-r, r)
 
-            matrix = []
-            cur = 0
-            for i in range(m):
-                cur = cur + 1
-                binar = bin(cur)[2:]
-                while len(binar) < r:
-                    binar = '0' + binar
-                matrix.append(list(binar))
+            pbits = self.__generateParityBits(data, matrix)
 
-            c = 0
-            pbits = []
+            syndrome = reduce(lambda x,y: str(x)+str(y), pbits)# position of error as binary code
+            error = int(syndrome,2) # converting binary to decimal
 
-            while 2 ** c < m:
-                ans = 0
-                for i in range(m):
-                    ans += int(data[i]) * int(matrix[i][c])
-                pbits.append( ans % 2 )
-                c += 1
-
-            syndrome = reduce(lambda x,y: str(x)+str(y), pbits)
-            error = int(syndrome,2)
-
-            if error == 0:
-                print("No errors finded")
-            else:
-                print('Error at: ' , error)
+            if error != 0:
                 data[error-1] = str(1 - int(data[error-1]))
+
             ans = ans = ''.join(map(str, data))
             return ans
         except Exception as e:
             print("Unknown error at hamming decode", e)
 
-    def printNodes(self):
+    def __HammingErrorCorrection(self):
         try:
-            for i in self.__codes:
-                if i != '\n':
-                    print(i, self.__codes[i])
-                else:
-                    print('endline', self.__codes[i])
+            # Error correcting of hamming
+            with open('WithErrors.txt', 'r') as file:
+                txt = file.read()
+                txt = txt.strip()
+
+            res = ''
+            for i in range(0, len(txt), 7):
+                x = list(txt[i:(i + 7)])
+                cur = self.__HammingBlockCorrection(i, x)
+                res += cur
+            print("Corrected hamming encoded text", res)
+            with open('corrected_hamming.txt', 'w') as file:
+                file.write(res)
         except Exception as e:
-            print("Unknown error: ", e)
+            print("HammingErrorCorrection unknown error: ", e)
+
